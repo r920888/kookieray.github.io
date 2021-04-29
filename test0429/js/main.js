f14904e077d8eed868aa4d5073ac96258fe5ab92
r920888 <r920888@hotmail.com>
2021-04-29 19:17:02 +0800

    0429

--- test0429/js/main.js
+++ test0429/js/main.js
@@ -1,40 +1,3 @@
-
-var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('testyt', {
-    height: '360',
-    width: '640',
-    videoId: 'UekmYMSMAPU',
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  });
-}
-
-function onPlayerReady(event) {
-  event.target.playVideo();
-}
-
-var done = false;
-function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
-  }
-}
-function stopVideo() {
-  player.stopVideo();
-}
-
-
-
 $(function(){
 
   if (window.innerWidth < 768) {
@@ -113,6 +76,7 @@ $('.join-container').paroller();
 /*------------------tab------------------*/
 
 $('.chara-container').slick({
+  lazyLoad: 'ondemand',
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: false,
